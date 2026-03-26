7f99deb6c87e020c23f13830218d35b2f735e58b
maddalayashwanth2017-lgtm <maddalayashwanth2017@gmail.com>
2026-03-27 00:24:07 +0530

    fro_loop

--- py_assignments/for_loops/sum of even numbers.py
+++ py_assignments/for_loops/sum of even numbers.py
@@ -1,17 +1,16 @@
-num=input("enter your number give comma after every number:-- ").split(",")
-x=[]
-y=[]
+x = input("Enter a number: ")
+y=x
+rev=0
+even=0
+odd=0
+for i in x:
+    rem = int(i)
 
-for i in num:
-    i=int(i)
-    if i%2==0:
-        x.append(i)
+    if rem%2==0:
+        even+=rem
     else:
-        y.append(i)
-
-print(f"sum of even num= {sum(x)} , sum of odd number= {sum(y)}")
-
-
-
-
+        odd+=rem
+   
+print(f"sum ofeven num are: {even}")
+print(f"sum ofeven num are: {odd}")
 
