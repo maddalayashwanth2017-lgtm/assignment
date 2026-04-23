b65e3b103755f6ae73ac5ee0f1f428e06fc2fe77
rocky <maddalayashwanth2017@gmail.com>
2026-04-23 18:47:08 +0530

    multiple

--- oops/inheritance/multiple.py
+++ oops/inheritance/multiple.py
@@ -1,17 +1,36 @@
-class Father:
+class Camera:
 
-    def height(self):
-        self.height_=5.11
-        print("father height is 5.11")
-    
-    def colour(self):
-        self.colour= "white"
+    def __init__(self,MP):
+        self.MP=MP
+    def take_photo(self):
+        print("photo captured")
+
+
          
-class Son(Father):
-    def run(self):
-        print("son runs faster ")
+class MusicPlayer:
+    def __init__(self,brand):
+        self.brand=brand
+    
+    def play_music(self):
+        print("Music started")
 
-class Daughter(Father):
-    def cook(self):
-        print("cookes better: ")
 
+
+class SmartPhone(Camera,MusicPlayer):
+    def __init__(self,MP,brand,model_name):
+        Camera.__init__(self,MP)
+        MusicPlayer.__init__(self,brand)
+        self.model_name=model_name
+
+    def show_details(self):
+        
+        print(self.MP,"MP")
+        print("brand =",self.brand)
+        print("model =",self.model_name)
+
+phone1=SmartPhone(48,"apple","17 pro max")
+phone2=SmartPhone(200,"samsung","S26 ultra")
+
+phone1.show_details()
+print()
+phone2.show_details()
